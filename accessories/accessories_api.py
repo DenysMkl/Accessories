151d46e4da82005dba15f95106068e4913eb24fa
DenysMkl <den.mikliaiev@gmail.com>
2024-01-03 01:35:58 +0200

    add pagination

--- accessories/accessories_api.py
+++ accessories/accessories_api.py
@@ -10,27 +10,19 @@ app = FastAPI()
 connect = MongoDB()
 
 
-@app.get('/')
-async def hello():
-    return {'hello': 'world!'}
+@app.get('/{item_type}', response_model=List)
+async def get_accessories(item_type: str,
+                          volume: int = 0,
+                          diagonal: float = 0,
+                          model: str = '',
+                          page_numb: int = 1,
+                          page_size: int = 10):
 
+    get_params = {'volume': volume,
+                  'diagonal': diagonal,
+                  'model': model}
+    start = (page_numb - 1) * page_size
+    end = start + page_size
+    data = entities.client_code(item_type, **get_params)
 
-@app.get('/cases', response_model=List)
-def get_cases():
-    db = connect.db_name
-    collection = db['Cases']
-    data = collection.find()
-    return list(data)
-
-# @app.get('/{item_type}', response_model=List)
-# async def get_accessories(item_type: str,
-#                           volume: int = 0,
-#                           diagonal: float = 0,
-#                           model: str = ''):
-#     get_params = {'volume': volume,
-#                   'diagonal': diagonal,
-#                   'model': model}
-#
-#     data = entities.client_code(item_type, **get_params)
-#
-#     return data.get_data(connect)
+    return data.get_data(connect)[start:end]
