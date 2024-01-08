02bf0ff4df523f8a5348803faaa10d028d498992
DenysMkl <den.mikliaiev@gmail.com>
2024-01-08 17:56:03 +0200

    rewrite json file

--- store_data/store_data.py
+++ store_data/store_data.py
@@ -9,8 +9,10 @@ database = MongoDB()
 
 def restore_data(db) -> None:
     for col_name in db.db_name.list_collection_names():
-        with open(f'../../data/mongo/{col_name}.json', 'w') as file:
-            json.dump(list(db.db_name[col_name].find({}, {})), fp=file, indent=4)
+        with open(f'../../data/mongo/{col_name}.json', 'a', encoding='utf-8') as file:
+            for obj in db.db_name[col_name].find({}, {}):
+                json.dump(obj, fp=file, ensure_ascii=False)
+                file.write('\n')
 
 
 if __name__ == '__main__':
