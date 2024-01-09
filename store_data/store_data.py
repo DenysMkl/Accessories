02d6503f7fcefaa119644e6a37e1f9949dd01201
DenysMkl <den.mikliaiev@gmail.com>
2024-01-09 14:09:19 +0200

    fix

--- store_data/store_data.py
+++ store_data/store_data.py
@@ -9,7 +9,8 @@ database = MongoDB()
 
 def restore_data(db) -> None:
     for col_name in db.db_name.list_collection_names():
-        with open(f'../../data/mongo/{col_name}.json', 'a', encoding='utf-8') as file:
+        with open(f'../../data/mongo/{col_name}.json', 'a',
+                  encoding='utf-8') as file:
             for obj in db.db_name[col_name].find({}, {}):
                 json.dump(obj, fp=file, ensure_ascii=False)
                 file.write('\n')
